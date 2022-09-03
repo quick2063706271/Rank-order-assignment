47289c4d5ce63a9f5e969ff6ea8c60f982458480
Kuai Yu <quick2063706271@outlook.com>
2022-09-03 16:42:32 -0400

    update Hungarian Algorithm

--- HungarianAlgorithm.py
+++ HungarianAlgorithm.py
@@ -1,58 +1,67 @@
 import numpy as np
 
 
-def min_zero_row(zero_mat, mark_zero):
-    '''
-    The function can be splitted into two steps:
+# Reference (Code develped from): https://python.plainenglish.io/hungarian-algorithm-introduction-python-implementation-93e7c0890e15
+
+def mark_min_zero_row(zero_mat, mark_zero):
+    """
+    The function can be split into two steps:
     #1 The function is used to find the row which containing the fewest 0.
     #2 Select the zero number on the row, and then marked the element corresponding row and column as False
-    '''
+    :param zero_mat:
+    :param mark_zero:
+    """
 
-    # Find the row
-    min_row = [99999, -1]
-
+    min_zero_row = [10000000, -1]  # Store [(number of 0), (row idx)]
+    # 1. find row that has least number of zeros
     for row_num in range(zero_mat.shape[0]):
-        if 0 < np.sum(zero_mat[row_num] == True) < min_row[0]:
-            min_row = [np.sum(zero_mat[row_num] == True), row_num]
+        num_zero = np.sum(zero_mat[row_num] == True)
+        if 0 < num_zero < min_zero_row[0]:
+            min_zero_row = [num_zero, row_num]
 
-    # Marked the specific row and column as False
-    zero_index = np.where(zero_mat[min_row[1]] == True)[0][0]
-    mark_zero.append((min_row[1], zero_index))
-    zero_mat[min_row[1], :] = False
+    # Store the marked coordinates into mark_zero
+    zero_index = np.where(zero_mat[min_zero_row[1]] == True)[0][0]
+    mark_zero.append((min_zero_row[1], zero_index))
+
+    # 2. Marked both row and column as False
+    zero_mat[min_zero_row[1], :] = False
     zero_mat[:, zero_index] = False
 
 
-def mark_matrix(mat):
-    '''
+def mark_matrix(matrix):
+    """
     Finding the returning possible solutions for LAP problem.
-    '''
+    :param matrix:
+    """
 
     # Transform the matrix to boolean matrix(0 = True, others = False)
-    cur_mat = mat
-    zero_bool_mat = (cur_mat == 0)
-    zero_bool_mat_copy = zero_bool_mat.copy()
+    cur_matrix = matrix
+    zero_boolean_matrix = (cur_matrix == 0)
+    zero_boolean_matrix_copy = zero_boolean_matrix.copy()
 
-    # Recording possible answer positions by marked_zero
-    marked_zero = []
-    while True in zero_bool_mat_copy:
-        min_zero_row(zero_bool_mat_copy, marked_zero)
+    # Store possible answer positions by marked_zero
+    marked_zero = []  # store [(idxes that marked zero), (row Idx)]
+    while True in zero_boolean_matrix_copy:
+        mark_min_zero_row(zero_boolean_matrix_copy, marked_zero)
 
-    # Recording the row and column positions seperately.
+    # Recording the row and column positions separately.
     marked_zero_row = []
     marked_zero_col = []
     for i in range(len(marked_zero)):
         marked_zero_row.append(marked_zero[i][0])
         marked_zero_col.append(marked_zero[i][1])
 
-    # Step 2-2-1
-    non_marked_row = list(set(range(cur_mat.shape[0])) - set(marked_zero_row))
+    # Store indexes of row that do not contain marked 0 elements
+    non_marked_row = list(set(range(cur_matrix.shape[0])) - set(marked_zero_row))
 
     marked_cols = []
     check_switch = True
     while check_switch:
         check_switch = False
+        # Search for any unmarked 0 elements in their column
         for i in range(len(non_marked_row)):
-            row_array = zero_bool_mat[non_marked_row[i], :]
+            row_array = zero_boolean_matrix[non_marked_row[i], :]
+
             for j in range(row_array.shape[0]):
                 # Step 2-2-2
                 if row_array[j] == True and j not in marked_cols:
@@ -67,64 +76,72 @@ def mark_matrix(mat):
                 non_marked_row.append(row_num)
                 check_switch = True
     # Step 2-2-6
-    marked_rows = list(set(range(mat.shape[0])) - set(non_marked_row))
+    marked_rows = list(set(range(matrix.shape[0])) - set(non_marked_row))
 
-    return (marked_zero, marked_rows, marked_cols)
+    return marked_zero, marked_rows, marked_cols
 
 
 def adjust_matrix(mat, cover_rows, cover_cols):
+    """
+    1. Find the minimum value that is not in marked_rows and marked_cols
+    2. Subtract the elements which are not in marked_rows nor marked_cols form the minimum values
+    3. Add the element in marked_rows to the min value
+    :param mat:
+    :param cover_rows:
+    :param cover_cols:
+    :return:
+    """
     cur_mat = mat
     non_zero_element = []
 
-    # Step 4-1
+    # 1. Find the minimum value that is not in marked_rows and marked_cols
     for row in range(len(cur_mat)):
         if row not in cover_rows:
             for i in range(len(cur_mat[row])):
                 if i not in cover_cols:
                     non_zero_element.append(cur_mat[row][i])
-    min_num = min(non_zero_element)
+    min_val = min(non_zero_element)
 
-    # Step 4-2
+    # 2. Subtract the elements which are not in marked_rows nor marked_cols form the minimum values
     for row in range(len(cur_mat)):
         if row not in cover_rows:
             for i in range(len(cur_mat[row])):
                 if i not in cover_cols:
-                    cur_mat[row, i] = cur_mat[row, i] - min_num
-    # Step 4-3
+                    cur_mat[row, i] = cur_mat[row, i] - min_val
+    # 3. Add the element in marked_rows to the min value
     for row in range(len(cover_rows)):
         for col in range(len(cover_cols)):
-            cur_mat[cover_rows[row], cover_cols[col]] = cur_mat[cover_rows[row], cover_cols[col]] + min_num
+            cur_mat[cover_rows[row], cover_cols[col]] = cur_mat[cover_rows[row], cover_cols[col]] + min_val
     return cur_mat
 
 
 def hungarian_algorithm(matrix):
+    """Return the result of linear assignment from matrix using hungarian algorithm
+
+    :param matrix:
+    :return:
+    """
+
     dimension = matrix.shape[0]
     cur_matrix = matrix
 
-    # Step 1 - Every column and every row subtract its internal minimum
-    for row_idx in range(matrix.shape[0]):
-        cur_matrix[row_idx] = cur_matrix[row_idx] - np.min(cur_matrix[row_idx])
-
+    # 1. Subtract every column and every row with its internal minimum
+    for row_idx in range(dimension):
+        min_val = np.min(cur_matrix[row_idx])
+        cur_matrix[row_idx] = cur_matrix[row_idx] - min_val
     for col_idx in range(matrix.shape[1]):
-        cur_matrix[:, col_idx] = cur_matrix[:, col_idx] - np.min(cur_matrix[:, col_idx])
-    zero_count = 0
-    while zero_count < dimension:
-        # Step 2 & 3
-        ans_pos, marked_rows, marked_cols = mark_matrix(cur_matrix)
-        zero_count = len(marked_rows) + len(marked_cols)
+        min_val = np.min(cur_matrix[:, col_idx])
+        cur_matrix[:, col_idx] = cur_matrix[:, col_idx] - min_val
 
+    zero_count = 0
+    # Repeat step 2 and 3 until the zero_count == dimension
+    result = None
+    while zero_count < dimension:
+        # 2
+        result, marked_rows, marked_cols = mark_matrix(cur_matrix)
+        zero_count = len(marked_cols) + len(marked_rows)
+        # 3
         if zero_count < dimension:
             cur_matrix = adjust_matrix(cur_matrix, marked_rows, marked_cols)
 
-    return ans_pos
-
-
-def ans_calculation(mat, pos):
-    total = 0
-    ans_mat = np.zeros((mat.shape[0], mat.shape[1]))
-    for i in range(len(pos)):
-        total += mat[pos[i][0], pos[i][1]]
-        ans_mat[pos[i][0], pos[i][1]] = mat[pos[i][0], pos[i][1]]
-    return total, ans_mat
-
-
+    return result
